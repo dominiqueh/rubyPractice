77b49890c85e2a264f5c50b74aa5511cffeb9729
dominiqueh <dominiquehorner@gmail.com>
2017-03-14 23:55:57 -0600

    concatenation and converting strings to integers + fiddling with ruby convention

--- first_program.rb
+++ first_program.rb
@@ -19,3 +19,11 @@ puts(greeting_phrase)
 puts("Please type in your name:")
 name = gets
 puts("Thank you " + name)
+
+# concatenation and converting strings to integers and toying w ruby convention
+cute_siblings = "Kyle, " + "Rhyse," + " Dylan"
+puts(cute_siblings)
+sibling_ages = "6" + "5" + "4"
+puts(sibling_ages)
+sibling_ages = "6".to_i + "5".to_i()+ "4".to_i
+puts(sibling_ages)
