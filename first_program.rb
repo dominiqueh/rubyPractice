4f1c714361012e7315fb6f1ab28b28564ce25ebb
dominiqueh <dominiquehorner@gmail.com>
2017-03-14 23:42:04 -0600

    put method, storing variables, and re-assignment

--- first_program.rb
+++ first_program.rb
@@ -1,4 +1,10 @@
-greeting = "Hello world!"
-puts(greeting)
-puts(greeting)
-puts(greeting)
+# storing a string as a variable and console logging strings with put method
+
+greeting_phrase = "Hello world!"
+puts(greeting_phrase)
+puts(greeting_phrase)
+puts(greeting_phrase)
+
+# re-assignment
+greeting_phrase = "Bonjour le monde!"
+puts(greeting_phrase)
